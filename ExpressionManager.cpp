36e2b4cc1cedc1c6fc4ccc8b74fbbdbce9cc152d
Sarra Smith <sarraelayne@gmail.com>
2018-10-01 02:47:16 +0000

    Basic Structure

---
+++ ExpressionManager.cpp
@@ -0,0 +1,17 @@
+#include"ExpressionManager.h"
+#include<iostream>
+#include<string>
+using namespace std;
+	
+bool ExpressionManager::isBalanced(string expression) {
+    return false;
+}
+string ExpressionManager::postfixToInfix(string postfixExpression) {
+    return inExpression;
+}	
+string ExpressionManager::postfixEvaluate(string postfixExpression){
+    return newInfix;
+}
+string ExpressionManager::infixToPostfix(string infixExpression){
+    return newPostfix;
+}
---
+++ ExpressionManager.h
@@ -0,0 +1,21 @@
+#include #pragma once
+#include "ExpressionManagerInterface.h"
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+class ExpressionManager: public ExpressionManagerInterface() {
+    public: 
+    ~ExpressionManagerInterface() {}
+    
+	bool isBalanced(string expression);
+	string postfixToInfix(string postfixExpression);
+	string postfixEvaluate(string postfixExpression);
+	string infixToPostfix(string infixExpression);
+	
+	protected:
+	string inExpression;
+	string newInfix;
+	string newPostfix;
+}
